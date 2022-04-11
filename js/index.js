96c5ac3374b9996456b8180a6a75733e3b77443a
Wendy <misswendymoira@gmail.com>
2022-04-10 23:59:58 -0700

    scrolls and buttons

---
+++ js/index.js
@@ -0,0 +1,28 @@
+
+
+function topScroll() {
+document.body.scrollTop = 0;
+}
+
+
+
+function aboutScroll() {
+    const abtscr = document.getElementById("scrollToAbout");
+    abtscr.scrollIntoView();
+}
+
+function workScroll() {
+    const wrkscr = document.getElementById("scrollToWork");
+    wrkscr.scrollIntoView();
+}
+
+
+function contactScroll() {
+    const cntscr = document.getElementById("scrollToContact");
+    cntscr.scrollIntoView();
+}
+
+// function flag4Scroll() {
+//     const fl4scr = document.getElementById("scrollToFlag4");
+//     fl4scr.scrollIntoView();
+// }
